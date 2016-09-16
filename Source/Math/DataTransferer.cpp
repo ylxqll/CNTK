346fbe7d3424dce8ae9f6748e5b576c07ad50c6b
Eldar Akchurin <eldak@microsoft.com>
2016-09-16 17:27:07 +0200

    Fixing compilation

--- Source/Math/DataTransferer.cpp
+++ Source/Math/DataTransferer.cpp
@@ -3,15 +3,15 @@
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
 
+#include "stdafx.h"
 #include "DataTransferer.h"
-//#include "GPUDataTransferer.h"
+#include "GPUDataTransferer.h"
 
 namespace Microsoft { namespace MSR { namespace CNTK {
 
-    DataTransfererPtr CreatePrefetchDataTransferer(int /*deviceId*/)
+    DataTransfererPtr CreatePrefetchDataTransferer(int deviceId)
     {
-        //return std::make_shared<PrefetchGPUDataTransferer>(deviceId);
-        return nullptr;
+        return std::make_shared<PrefetchGPUDataTransferer>(deviceId);
     }
 
 } } }
